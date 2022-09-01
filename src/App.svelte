7036b285e6c442ab7a63473f43a8cbed5dc3fcfa
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-01 18:29:56 +0100

    Added map to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,40 @@
 <script>
 	export let name;
+	import { onMount } from "svelte";
+	import L from "leaflet";
+
+  // Initialise Map
+  let map;
+  let marker;
+  onMount(() => {
+	fetch("/api/oauth-function").then((response) => response.json()).then((response) => alert(response))
+
+	var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '© OpenStreetMap'
+	});
+	    map = L.map("map", { zoomControl: false,
+			center: [51.505, -0.09],
+    		zoom: 13,
+			layers: [osm]
+		});
+    L.control.zoom({ position: "topright" }).addTo(map);
+
+  });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div id="map" style="position: fixed" />
 
 <style>
+  #map {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+
 	main {
 		text-align: center;
 		padding: 1em;
@@ -28,3 +55,6 @@
 		}
 	}
 </style>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
