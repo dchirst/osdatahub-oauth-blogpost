cb672d534908f6d5b2987302f6bd9a73918cd54e
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-10 13:06:50 +0100

    Updated Azure function to rererence key vault

--- api/oauthfunction/__init__.py
+++ api/oauthfunction/__init__.py
@@ -5,16 +5,18 @@ from requests_oauthlib import OAuth2Session
 import azure.functions as func
 from azure.keyvault.secrets import SecretClient
 import json
-from azure.identity import ManagedIdentityCredential
+from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
 
 token_url = "https://api.os.uk/oauth2/token/v1"
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    identity = ManagedIdentityCredential()
+    identity = DefaultAzureCredential()
     logging.info('Python HTTP trigger function processed a request.')
     secretClient = SecretClient(vault_url="https://dhirst-kv.vault.azure.net/", credential=identity)
-    project_api_key = secretClient.get_secret('project-api-key')
-    client_secret = secretClient.get_secret('client-secret')
+    project_api_key = secretClient.get_secret('project-api-key').value
+
+    client_secret = secretClient.get_secret('client-secret').value
+    
     client = BackendApplicationClient(client_id=project_api_key)
     oauth = OAuth2Session(client=client)
     token = oauth.fetch_token(token_url=token_url, client_id=project_api_key,
