2049ce58eeb097b8de255e0bffb95569c68ec06f
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-10 15:16:57 +0100

    Added most basic error handling

--- api/oauthfunction/__init__.py
+++ api/oauthfunction/__init__.py
@@ -10,16 +10,20 @@ from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
 token_url = "https://api.os.uk/oauth2/token/v1"
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    identity = DefaultAzureCredential()
-    logging.info('Python HTTP trigger function processed a request.')
-    secretClient = SecretClient(vault_url="https://dhirst-kv.vault.azure.net/", credential=identity)
-    project_api_key = secretClient.get_secret('project-api-key').value
+    try:
+        identity = DefaultAzureCredential()
+        logging.info('Python HTTP trigger function processed a request.')
+        secretClient = SecretClient(vault_url="https://dhirst-kv.vault.azure.net/", credential=identity)
+        project_api_key = secretClient.get_secret('project-api-key').value
 
-    client_secret = secretClient.get_secret('client-secret').value
-    
-    client = BackendApplicationClient(client_id=project_api_key)
-    oauth = OAuth2Session(client=client)
-    token = oauth.fetch_token(token_url=token_url, client_id=project_api_key,
-                              client_secret=client_secret)
+        client_secret = secretClient.get_secret('client-secret').value
 
-    return json.dumps(token)
+        client = BackendApplicationClient(client_id=project_api_key)
+        oauth = OAuth2Session(client=client)
+        token = oauth.fetch_token(token_url=token_url, client_id=project_api_key,
+                                  client_secret=client_secret)
+
+        return json.dumps(token)
+
+    except Exception as e:
+        return func.HttpResponse(str(e), status_code=500)
