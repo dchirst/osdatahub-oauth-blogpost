a31fa4de880294dcfe2112b6d0227aaa0d262952
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-10 17:29:30 +0100

    Added alternative way of accessing client_secret and project_api_key

--- api/oauthfunction/__init__.py
+++ api/oauthfunction/__init__.py
@@ -6,6 +6,7 @@ import azure.functions as func
 from azure.keyvault.secrets import SecretClient
 import json
 from azure.identity import ManagedIdentityCredential, DefaultAzureCredential
+from os import environ
 
 token_url = "https://api.os.uk/oauth2/token/v1"
 
@@ -19,14 +20,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         project_api_key = secretClient.get_secret('project-api-key').value
 
         client_secret = secretClient.get_secret('client-secret').value
-        logging.info("Got api key and secret")
-        client = BackendApplicationClient(client_id=project_api_key)
-        oauth = OAuth2Session(client=client)
-        logging.info("Connected to oauth client")
-        token = oauth.fetch_token(token_url=token_url, client_id=project_api_key,
-                                  client_secret=client_secret)
-        logging.info('Got token')
-        return json.dumps(token)
+    except:
+        project_api_key = environ.get("project_api_key")
+        client_secret = environ.get("client_secret")
+
+    logging.info("Got api key and secret")
+    client = BackendApplicationClient(client_id=project_api_key)
+    oauth = OAuth2Session(client=client)
+    logging.info("Connected to oauth client")
+    token = oauth.fetch_token(token_url=token_url, client_id=project_api_key,
+                              client_secret=client_secret)
+    logging.info('Got token')
+    return json.dumps(token)
 
     except Exception as e:
         return func.HttpResponse(str(e), status_code=500)
