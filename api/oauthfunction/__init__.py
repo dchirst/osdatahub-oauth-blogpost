8b9a1deae6568da93c2911d9cfc2d5f5f70e5793
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-09-11 19:29:48 +0100

    Removed getting environment variable

--- api/oauthfunction/__init__.py
+++ api/oauthfunction/__init__.py
@@ -12,18 +12,14 @@ token_url = "https://api.os.uk/oauth2/token/v1"
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        try:
-            logging.info("Running oauth function")
-            identity = DefaultAzureCredential()
-            logging.info('Python HTTP trigger function processed a request.')
-            secretClient = SecretClient(vault_url="https://dhirst-kv.vault.azure.net/", credential=identity)
-            logging.info("Authenticated")
-            project_api_key = secretClient.get_secret('project-api-key').value
+        logging.info("Running oauth function")
+        identity = DefaultAzureCredential()
+        logging.info('Python HTTP trigger function processed a request.')
+        secretClient = SecretClient(vault_url="https://dhirst-kv.vault.azure.net/", credential=identity)
+        logging.info("Authenticated")
+        project_api_key = secretClient.get_secret('project-api-key').value
 
-            client_secret = secretClient.get_secret('client-secret').value
-        except:
-            project_api_key = environ.get("project_api_key")
-            client_secret = environ.get("client_secret")
+        client_secret = secretClient.get_secret('client-secret').value
 
         logging.info("Got api key and secret")
         client = BackendApplicationClient(client_id=project_api_key)
